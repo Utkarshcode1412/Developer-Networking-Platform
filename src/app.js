55236e2a5dfb5e11640f2fd2c25577b609486383
Utkarshcode1412 <pawarutkarsh579@gmail.com>
2026-06-25 01:27:39 +0530

    add validateProfileEdit function to check allowed fields in profile edit requests

--- src/app.js
+++ src/app.js
@@ -1,55 +1,17 @@
 const express = require("express")
 const connectDB = require("./config/database.js")
-const User = require("./models/user.js");
-const app = express();
-const { validateSignupData } = require("./utils/validation.js");
-const bcrypt = require("bcrypt");
-const jsonwebtoken = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
-const { userAuth } = require("./middleware/auth.js");
-
+const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 
 
+const authRouter = require("./routes/registerAuth.js");
+const profileRouter = require("./routes/profile.js");
 
-
-
-
-
-
-app.get("/user", async(req, res) => {
-    const userEmail = req.body.emailId;
-
-    try {
-        const user = await User.findOne({emailId: userEmail});
-
-        if(user.length === 0) {
-            res.status(404).send("user not found");
-        }
-        else {
-            res.send(user);
-        }
-    } catch (error) {
-        res.status(400).send("something went wrong");
-    }
-})
-
-app.get("/feed", async(req, res) => {
-    try {
-        const users = await User.find({});
-
-        if(users.length === 0) {
-            res.status(404).send("user not found");
-        }
-        else {
-            res.send(users);
-        }
-    } catch (error) {
-         res.status(400).send("something went wrong");
-    }
-})
+app.use("/", authRouter);
+app.use("/", profileRouter);
 
 
 
