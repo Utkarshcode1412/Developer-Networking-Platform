3b065ef1705bed1f0de288cce7571396e4a05a5e
Utkarshcode1412 <pawarutkarsh579@gmail.com>
2026-06-27 11:15:31 +0530

    added pagination in feed api with limit and page query params

--- src/routes/users.js
+++ src/routes/users.js
@@ -69,6 +69,12 @@ userRouter.get("/user/feed", userAuth, async (req, res) => {
         
         const loggedInUser = req.user;
 
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+
+        limit = limit > 50 ? 50 : limit;
+        const skip = (page-1) * limit;
+
         const connectionRequest = await ConnectionRequest.find(
             {
                 $or: [
@@ -88,11 +94,19 @@ userRouter.get("/user/feed", userAuth, async (req, res) => {
         const users = await User.find(
             {
                 $and: [
-                    { _id: { $nin: Array.from(hideUserFromFeed)}},
-                    { _id: {$ne: loggedInUser._id}}
+                    {
+                        _id: { 
+                            $nin: [
+                                ...hideUserFromFeed, loggedInUser._id
+                            ]
+                        }
+                    }
                 ]
             }
-        ).select(SAFE_USER_DATA);
+        )
+        .select(SAFE_USER_DATA)
+        .skip(skip)
+        .limit(limit);
 
         res.json(
             {
